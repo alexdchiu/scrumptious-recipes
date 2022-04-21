0607136684485519e1f02f304e9198a8f5a0f86c
Alex Chiu <alexdchiu@gmail.com>
2022-04-21 15:54:38 -0400

    further cleanup

--- recipes/admin.py
+++ recipes/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from recipes.models import Recipe, Measure, FoodItem, Ingredient, Step
+from recipes.models import Recipe, Measure, FoodItem, Ingredient, Step, Rating
 
 
 # Register your models here.
@@ -23,9 +23,12 @@ class IngredientAdmin(admin.ModelAdmin):
 class StepAdmin(admin.ModelAdmin):
     pass
 
+class RatingAdmin(admin.ModelAdmin):
+    pass
 
 admin.site.register(Recipe, RecipeAdmin)
 admin.site.register(Measure, MeasureAdmin)
 admin.site.register(FoodItem, FoodItemAdmin)
 admin.site.register(Ingredient, IngredientAdmin)
 admin.site.register(Step, StepAdmin)
+admin.site.register(Rating, RatingAdmin)
