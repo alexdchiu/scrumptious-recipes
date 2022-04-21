d086f5d723ada00f07d3839ae933ab62f0151d41
Alex Chiu <alexdchiu@gmail.com>
2022-04-21 13:59:20 -0400

    Updated from ZIP file

--- recipes/admin.py
+++ recipes/admin.py
@@ -2,21 +2,27 @@ from django.contrib import admin
 
 from recipes.models import Recipe, Measure, FoodItem, Ingredient, Step
 
+
 # Register your models here.
 class RecipeAdmin(admin.ModelAdmin):
-  pass
+    pass
+
 
 class MeasureAdmin(admin.ModelAdmin):
-  pass
+    pass
+
 
 class FoodItemAdmin(admin.ModelAdmin):
-  pass
+    pass
+
 
 class IngredientAdmin(admin.ModelAdmin):
-  pass
+    pass
+
 
 class StepAdmin(admin.ModelAdmin):
-  pass
+    pass
+
 
 admin.site.register(Recipe, RecipeAdmin)
 admin.site.register(Measure, MeasureAdmin)
